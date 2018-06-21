3c19085287f22601bab67a8fbbf4ac16f02c4fae
KJankowska <39523923+KJankowska@users.noreply.github.com>
2018-06-21 17:06:23 +0200

    Add files via upload

---
+++ skeleton/src/com/wildhogs/Main.java
@@ -0,0 +1,9 @@
+package com.wildhogs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu("Menu", "Home", "Weeklist", "KitchenBook");
+        menu.printMenu();
+    }
+}
