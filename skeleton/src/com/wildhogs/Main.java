5ea4d457841044afa0c1de539d167efe74b188b1
KJankowska <39523923+KJankowska@users.noreply.github.com>
2018-06-22 16:07:13 +0200

    Delete Main.java

--- skeleton/src/com/wildhogs/Main.java
+++
@@ -1,9 +0,0 @@
-package com.wildhogs;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Menu menu = new Menu("Menu", "Home", "Weeklist", "KitchenBook");
-        menu.printMenu();
-    }
-}
