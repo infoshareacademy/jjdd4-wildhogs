61275576c4b017fb5f582d9849398eeea0d705f7
Natalia Dorozynska <natala.dorozynska@gmail.com>
2018-06-25 15:23:47 +0200

    Exceptions modified

--- src/main/java/com/infoshareacademy/jjdd4/wildhogs/JSONProvider.java
+++ src/main/java/com/infoshareacademy/jjdd4/wildhogs/JSONProvider.java
@@ -2,10 +2,8 @@ package com.infoshareacademy.jjdd4.wildhogs;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import java.io.FileNotFoundException;
+
 import java.io.FileReader;
-import java.io.IOException;
 
 public class JSONProvider {
 
@@ -16,18 +14,9 @@ public class JSONProvider {
         try {
             jsonObject = (JSONObject) parser.parse(new FileReader(source.getSource()));
 
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found!");
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println("IOException");
-            e.printStackTrace();
-        } catch (ParseException e) {
-            System.out.println("ParseException");
-            e.printStackTrace();
         } catch (Exception e) {
-            System.out.println("Exception e");
-            e.printStackTrace();
+            System.out.println("File not found!");
+            System.out.println(e.getMessage());
         }
         return jsonObject;
     }
