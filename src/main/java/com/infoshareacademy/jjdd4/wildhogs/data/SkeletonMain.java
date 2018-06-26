55eedccc2a408fe34b208fe96422c626728f39be
KJankowska <39523923+KJankowska@users.noreply.github.com>
2018-06-26 09:13:24 +0200

    Add files via upload

---
+++ src/main/java/com/infoshareacademy/jjdd4/wildhogs/data/SkeletonMain.java
@@ -0,0 +1,12 @@
+
+package com.infoshareacademy.jjdd4.wildhogs;
+
+
+public class SkeletonMain {
+    public SkeletonMain() {
+    }
+    public static void main(String[] args) {
+        SkeletonMenu menu = new SkeletonMenu("Menu", "Home", "Weeklist", "KitchenBook");
+        menu.printSkeletonMenu();
+    }
+}
