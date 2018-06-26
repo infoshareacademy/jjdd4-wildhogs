35b170671dd85bac552d560dbc13d85ef97f9717
Katarzyna Jankowska <j.jak.jankowska@gmail.com>
2018-06-26 16:39:03 +0200

    Change void name

--- src/main/java/com/infoshareacademy/jjdd4/wildhogs/app/Main.java
+++ src/main/java/com/infoshareacademy/jjdd4/wildhogs/app/Main.java
@@ -3,10 +3,19 @@ package com.infoshareacademy.jjdd4.wildhogs.app;
 import com.infoshareacademy.jjdd4.wildhogs.data.SkeletonMenu;
 import com.infoshareacademy.jjdd4.wildhogs.logic.MealCreator;
 
+import javax.security.auth.login.AppConfigurationEntry;
+import javax.security.auth.login.Configuration;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        new Configuration();
+        new Configuration() {
+            @Override
+            public AppConfigurationEntry[] getAppConfigurationEntry(String name) {
+                return new AppConfigurationEntry[0];
+            }
+        };
 
         MealCreator mealCreator = new MealCreator();
 
