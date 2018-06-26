1a256a4950df5a96822f3b3849c5ab24d8fedfde
Katarzyna Jankowska <j.jak.jankowska@gmail.com>
2018-06-26 16:33:04 +0200

    Configuration configuration

--- src/main/java/com/infoshareacademy/jjdd4/wildhogs/logic/JSONProvider.java
+++ src/main/java/com/infoshareacademy/jjdd4/wildhogs/logic/JSONProvider.java
@@ -8,13 +8,13 @@ import java.io.FileReader;
 
 public class JSONProvider {
 
-    public static JSONObject read(Configuration jsonSource) {
+    public static JSONObject read(Configuration configuration) {
 
         JSONParser parser = new JSONParser();
         JSONObject jsonObject = new JSONObject();
 
         try {
-            jsonObject = (JSONObject) parser.parse(new FileReader(jsonSource.getJsonPath()));
+            jsonObject = (JSONObject) parser.parse(new FileReader(configuration.getJsonPath()));
 
         } catch (Exception e) {
             System.out.println("File not found!");
