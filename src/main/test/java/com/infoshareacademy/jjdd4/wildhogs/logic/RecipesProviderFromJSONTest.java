40b6c50004f3fc96ae5810d4797ed2b5c9260ab0
Katarzyna Jankowska <j.jak.jankowska@gmail.com>
2018-06-28 16:53:49 +0200

    Tests MapOfMeal Demo

--- src/main/test/java/com/infoshareacademy/jjdd4/wildhogs/logic/RecipesProviderFromJSONTest.java
+++ src/main/test/java/com/infoshareacademy/jjdd4/wildhogs/logic/RecipesProviderFromJSONTest.java
@@ -1,50 +1,27 @@
 package com.infoshareacademy.jjdd4.wildhogs.logic;
 
 import com.infoshareacademy.jjdd4.wildhogs.app.Configuration;
-import com.infoshareacademy.jjdd4.wildhogs.data.Recipe;
-import net.bytebuddy.dynamic.loading.ClassInjector;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
 
-import java.util.Objects;
-import java.util.Properties;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static java.util.Objects.isNull;
-import static org.assertj.core.api.Assertions.assertThat;
-
-class RecipesProviderFromJSONTest {
-
-    //4 testy
-    // - czy zwraca nulla jakjest zła kategoria -> Bad category in recipes file: " + jsonObject.get("name").toString(
-    // - zła gramatura (300 amount) - > ad amount in recipes file in: " + recipe.getName
-    // - zła jednostka (gram ) -> Bad unit in recipes file: " + recipe.getName(
-    // - czy jak wszystko jest ok zwraca przepis  -> return recipe;
+class MealCreatorTest {
 
     @Test
-    void readsRecipesProviderAsNull() {
+    void getMapOfMeals() {
+        Configuration configuration = new Configuration();
+        JSONObject jsonObject = JSONProvider.read(configuration);
+        JSONArray recipesArray = (JSONArray) jsonObject.get("recipes");
 
-       Properties properties = new Properties();
-        JSONProvider json = new JSONProvider();
-        Recipe recipe = new Recipe(readsRecipesProviderAsNull());
-        JSONObject jsonObject = new JSONObject();
-        assertThat( jsonObject.get( "Bad category in recipes file:" +  jsonObject.get("name").toString());
+    }
 
+    @Test
+    void setMapOfMeals() {
+        Configuration configuration = new Configuration();
+        JSONObject jsonObject = JSONProvider.read(configuration);
+        JSONArray recipesArray = (JSONArray) jsonObject.get("recipes");
 
     }
 }
-
-//    @Test
-//    void ReturnEmptyObject() {
-//        Properties properties = new Properties();
-//        properties.setProperty( "jsonPath", "invalid_file" );
-//
-//        Configuration configuration = new Configuration( properties );
-//        JSONProvider json = new JSONProvider();
-//
-//        JSONObject jsonObject = json.read( configuration );
-//
-//        assertThat( jsonObject ).isNotNull();
-//        assertThat( jsonObject.isEmpty() ).isTrue();
-//
-//
