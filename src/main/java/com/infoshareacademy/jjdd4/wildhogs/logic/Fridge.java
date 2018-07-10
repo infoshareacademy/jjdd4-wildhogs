3441f89658e465f0ccc6b6aa099d02c3d30209bd
Natalia Dorozynska <natala.dorozynska@gmail.com>
2018-07-10 22:53:20 +0200

    sorting recipes by fridge contents

---
+++ src/main/java/com/infoshareacademy/jjdd4/wildhogs/logic/Fridge.java
@@ -0,0 +1,50 @@
+package com.infoshareacademy.jjdd4.wildhogs.logic;
+
+import com.infoshareacademy.jjdd4.wildhogs.data.Ingredient;
+import com.infoshareacademy.jjdd4.wildhogs.data.Recipe;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Fridge {
+
+    private List<String> ingridientsThatWeHave = Arrays.asList("chicken", "xyz", "mayo");
+    private MealCreator mealCreator = new MealCreator();
+
+
+    public void showFilterRecipe() {
+
+        List<Recipe> recipes = mealCreator.getMapOfMeals().entrySet().stream().map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+
+        List<RecipeMatchQuality> recipesWithMatchQuality = recipes.stream().map(recipe ->
+                new RecipeMatchQuality(recipe, recipeMatchQuality(recipe, ingridientsThatWeHave)))
+                .sorted((r1, r2) -> r1.matchQuality > r2.matchQuality ? -1 : 1)
+                .collect(Collectors.toList());
+
+        for (RecipeMatchQuality recipe: recipesWithMatchQuality) {
+            System.out.println(recipe.recipe.getName() + " " + recipe.matchQuality);
+        }
+    }
+
+    private class RecipeMatchQuality {
+        public Recipe recipe;
+        public double matchQuality;
+
+        public RecipeMatchQuality(Recipe recipe, double matchQuality) {
+            this.recipe = recipe;
+            this.matchQuality = matchQuality;
+        }
+    }
+
+     private double recipeMatchQuality(Recipe recipe, List<String> ingredients) {
+     Set<String> ingridientsInRecipe = recipe.getMap().keySet();
+     double matchQuality = ingridientsInRecipe.stream().filter(recipeIngredient->
+             ingredients.stream().anyMatch(recipeIngredient::contains)).count();
+     matchQuality /= ingredients.size();
+     return matchQuality;
+    }
+
+
+
+}
