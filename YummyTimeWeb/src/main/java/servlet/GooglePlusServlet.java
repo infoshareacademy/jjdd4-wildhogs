75095dd62b040851d67d52703f7c10e179b9cbef
mateusz mazur <matmazur90@gmail.com>
2018-07-30 15:54:55 +0200

    created Google2Api java file

--- YummyTimeWeb/src/main/java/servlet/GooglePlusServlet.java
+++ YummyTimeWeb/src/main/java/servlet/GooglePlusServlet.java
@@ -1,7 +1,8 @@
 package servlet;
 
-
-import org.jboss.msc.service.ServiceBuilder;
+import googleApi.Google2Api;
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.oauth.OAuthService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,28 +15,28 @@ import java.io.IOException;
 @WebServlet("/googleplus")
 public class GooglePlusServlet extends HttpServlet {
 
-//    private static final String CLIENT_ID = "client id here";
-//    private static final String CLIENT_SECRET = "client secret here";
-//
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse res)
-//            throws IOException, ServletException {
-//
-//        //Configure
-//        ServiceBuilder builder= new ServiceBuilder();
-//        OAuthService service = builder.provider(Google2Api.class)
-//                .apiKey(CLIENT_ID)
-//                .apiSecret(CLIENT_SECRET)
-//                .callback("http://localhost:8080/mywebapp/oauth2callback&quot;)
-//                        .scope("openid profile email " +
-//                                        "https://www.googleapis.com/auth/plus.login " +
-//                                “https://www.googleapis.com/auth/plus.me”)
-//         .debug(System.out)
-//                .build(); //Now build the call
-//
-//        HttpSession sess = req.getSession();
-//        sess.setAttribute("oauth2Service", service);
-//
-//        resp.sendRedirect(service.getAuthorizationUrl(null));
-//    }
+    private static final String CLIENT_ID = "907007146976-gj3molj87dk4k7jntu3hslrdubmd1947.apps.googleusercontent.com\n";
+    private static final String CLIENT_SECRET = "SMVB1yujYm_TGVqxaWRX5n43";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException {
+
+        //Configure
+        ServiceBuilder builder= new ServiceBuilder();
+        OAuthService service = builder.provider(Google2Api.class)
+                .apiKey(CLIENT_ID)
+                .apiSecret(CLIENT_SECRET)
+                .callback("http://localhost:8080/mywebapp/oauth2callback&quot;")
+                        .scope("openid profile email " +
+                                        "https://www.googleapis.com/auth/plus.login " +
+                                "https://www.googleapis.com/auth/plus.me")
+         .debug(System.out)
+                .build(); //Now build the call
+
+        HttpSession sess = req.getSession();
+        sess.setAttribute("oauth2Service", service);
+
+        res.sendRedirect(service.getAuthorizationUrl(null));
+    }
 }
