d90822d436d7d0ca761b952362014b3a6db0cf97
Natalia Dorozynska <natala.dorozynska@gmail.com>
2018-08-02 16:29:08 +0200

    first mail send

--- YummyTimeWeb/src/main/java/servlet/MailServlet.java
+++ YummyTimeWeb/src/main/java/servlet/MailServlet.java
@@ -1,11 +1,5 @@
 package servlet;
 
-import org.simplejavamail.email.Email;
-import org.simplejavamail.email.EmailBuilder;
-import org.simplejavamail.mailer.Mailer;
-import org.simplejavamail.mailer.MailerBuilder;
-import org.simplejavamail.mailer.config.TransportStrategy;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,10 +7,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.security.Principal;
+import com.sendgrid.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @WebServlet("/mail")
 public class MailServlet extends HttpServlet {
+
+    private static Logger logger = LoggerFactory.getLogger(SearchRecipesServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -25,22 +24,27 @@ public class MailServlet extends HttpServlet {
         PrintWriter writer = resp.getWriter();
         writer.println("test2");
 
+        Email from = new Email("test@example.com");
+        String subject = "Sending with SendGrid is Fun";
+        logger.info("from");
+        Email to = new Email("matmazur90@gmail.com");
+        logger.info("from");
+        Content content = new Content("text/plain", "<3 ;) your shopping list");
+        Mail mail = new Mail(from, subject, to, content);
 
-        Email email = EmailBuilder.startingBlank()
-                    .from("YummyTimeApp", "YummyTimeApp@gmail.com")
-                    .to("Mateusz", "nwe.nat@gmail.com")
-                    .withSubject("hey")
-                    .withPlainText("<3  ;)")
-                    .buildEmail();
+        SendGrid sg = new SendGrid("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");
+        Request request = new Request();
 
-        Mailer mailer = MailerBuilder
-                .withSMTPServer("smtp.gmail.com", 587, "YummyTimeApp", "gulasz520")
-                .withTransportStrategy(TransportStrategy.SMTP_TLS)
-                .withDebugLogging(true)
-                .buildMailer();
-            mailer.sendMail(email);
-
-
-
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            throw ex;
+        }
     }
 }
