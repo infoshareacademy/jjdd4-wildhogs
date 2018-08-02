70cd4791377397414e9e4d8b7fda8d484ff19242
Natalia Dorozynska <natala.dorozynska@gmail.com>
2018-08-02 16:50:32 +0200

    next change

--- YummyTimeWeb/src/main/java/servlet/MailServlet.java
+++ YummyTimeWeb/src/main/java/servlet/MailServlet.java
@@ -1,13 +1,14 @@
 package servlet;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import com.sendgrid.*;
+import dao.ShoppingListOfUserDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,19 +18,25 @@ public class MailServlet extends HttpServlet {
 
     private static Logger logger = LoggerFactory.getLogger(SearchRecipesServlet.class);
 
+    @Inject
+    ShoppingListOfUserDao shoppingListOfUserDao;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        StringBuilder sb = new StringBuilder();
+        sb.append("Your shopping list : \n\r");
 
-        PrintWriter writer = resp.getWriter();
-        writer.println("test2");
+        shoppingListOfUserDao.getIngridientsInShoppingListOfUser().stream()
+                .forEach(ingredient -> sb.append(ingredient.toString() + "\n\r"));
+        sb.append("\n\r Cheers!");
+        String mailText = sb.toString();
 
-        Email from = new Email("test@example.com");
-        String subject = "Sending with SendGrid is Fun";
-        logger.info("from");
-        Email to = new Email("matmazur90@gmail.com");
-        logger.info("from");
-        Content content = new Content("text/plain", "<3 ;) your shopping list");
+        Email from = new Email("YummyTime@App.com");
+        String subject = "Shopping List";
+        logger.info("Send mail");
+        Email to = new Email("nwe.nat@gmail.com");
+        Content content = new Content("text/plain", mailText);
         Mail mail = new Mail(from, subject, to, content);
 
         SendGrid sg = new SendGrid("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");
@@ -44,6 +51,7 @@ public class MailServlet extends HttpServlet {
             System.out.println(response.getBody());
             System.out.println(response.getHeaders());
         } catch (IOException ex) {
+            logger.info("Send mail falid");
             throw ex;
         }
     }
