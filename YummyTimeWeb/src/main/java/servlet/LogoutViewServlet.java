1d3aee13d045ca4d3d8f0fd0c2c279faa2c6a5ac
Mateusz Mazur <matmazur90@gmail.com>
2018-08-03 11:35:02 +0200

    google oauth2 to develop (#59)
    * possible solution for fixing ubuntu dependency problem with surefire:tests - possible suspect simple jason and junit4 instead of junit 5
    
    * created Google2Api java file
    
    * changed  debug to debugStream in googlePlusServlet
    
    * added scripts in header.html, IdToken... in googleApi package, changed GooglePlusServlet
    
    * changes in header
    
    * changes in persistence.xml, added user to db
    
    * saves users to db
    
    * total logout
    
    * mid work on logout button
    
    * additional changes / script etc
    
    * logging out works - it's not a masterpiece but come on + additional link when database is empty to upload page
    
    * removed duplicates users in database
    
    * proper comeback to the welcome page after 1 sec from logout
    
    * cosmetic changes
    
    * removed commented code from web pom

---
+++ YummyTimeWeb/src/main/java/servlet/LogoutViewServlet.java
@@ -0,0 +1,45 @@
+package servlet;
+
+import dao.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/logout-gmail")
+public class LogoutViewServlet extends HttpServlet {
+
+    Logger logger = LoggerFactory.getLogger(LogoutViewServlet.class);
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        Template template = templateProvider.getTemplate(getServletContext(), "logout.ftlh");
+
+        try {
+            template.process(new Object(), resp.getWriter());
+
+            HttpSession session = req.getSession(true);
+            session.setAttribute("logged", false);
+
+
+        } catch (TemplateException e) {
+            e.printStackTrace();
+            logger.warn("Can't load template");
+        }
+    }
+}
